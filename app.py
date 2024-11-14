351790b680fe8056013b93a1d96ddb26b20368a8
+futurehendrix <bochka13@list.ru>
2024-11-14 15:26:18 +0300

    add validations

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from utils import format_date
 from models import (
     create_table,
     add_transaction,
@@ -6,6 +7,7 @@ from models import (
     calculate_balance,
     delete_transaction,
     update_transaction,
+    get_category_stats,
 )
 import sqlite3
 
@@ -16,43 +18,63 @@ create_table()
 
 @app.route('/')
 def index():
+    category_stats = get_category_stats()
     transactions = get_transactions()
     balance = calculate_balance()
-    return render_template('index.html', transactions=transactions, balance=balance)
+    return render_template('index.html', transactions=transactions, balance=balance, category_stats=category_stats, format_date=format_date)
 
 @app.route('/add_transaction', methods=['POST'])
 def add_transaction_route():
-    amount = float(request.form['amount'])
-    type = request.form['type']
-    category = request.form['category']
-    date = request.form['date']
+    try:
+        amount = float(request.form['amount'])
+        if amount <= 0:
+            raise ValueError("Amount must be greater than zero.")
+        type = request.form['type']
+        category = request.form['category']
+        date = request.form['date']
 
-    add_transaction(amount, type, category, date)
-    return redirect(url_for('index'))
+        # Добавляем транзакцию в базу данных
+        add_transaction(amount, type, category, date)
+        return redirect(url_for('index'))
+    except ValueError as e:
+        return render_template('index.html', error_message=str(e), transactions=get_transactions(),
+                               balance=calculate_balance())
+
 
 @app.route('/delete_transaction/<int:transaction_id>', methods=['GET'])
 def delete_transaction_route(transaction_id):
     delete_transaction(transaction_id)
     return redirect(url_for('index'))
 
+
 @app.route('/edit_transaction/<int:transaction_id>', methods=['GET', 'POST'])
 def edit_transaction(transaction_id):
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM transactions WHERE id = ?', (transaction_id,))
-    transaction = cursor.fetchone()
-    conn.close()
-
     if request.method == 'POST':
-        amount = float(request.form['amount'])
-        type = request.form['type']
-        category = request.form['category']
-        date = request.form['date']
-        update_transaction(transaction_id, amount, type, category, date)
-        return redirect(url_for('index'))
+        try:
+            amount = float(request.form['amount'])
+            if amount <= 0:
+                raise ValueError("Amount must be greater than zero.")
+            type = request.form['type']
+            category = request.form['category']
+            date = request.form['date']
 
-    return render_template('edit_transaction.html', transaction=transaction)
-
+            # Обновляем транзакцию в базе данных
+            update_transaction(transaction_id, amount, type, category, date)
+            return redirect(url_for('index'))
+        except ValueError as e:
+            conn = sqlite3.connect(DB_NAME)
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM transactions WHERE id = ?', (transaction_id,))
+            transaction = cursor.fetchone()
+            conn.close()
+            return render_template('edit_transaction.html', transaction=transaction, error_message=str(e))
+    else:
+        conn = sqlite3.connect(DB_NAME)
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM transactions WHERE id = ?', (transaction_id,))
+        transaction = cursor.fetchone()
+        conn.close()
+        return render_template('edit_transaction.html', transaction=transaction)
 
 
 if __name__ == '__main__':
